e01afee8723f6da19dc37658cf2748abf3d6a0bb
Karishma <karivin>
2017-03-03 22:46:34 +0000

    create a class and a failed test

---
+++ FizBuzTempTests/FizzBuzzTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizBuzTemp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizBuzTemp.Tests
+{
+    [TestClass()]
+    public class FizzBuzzTests
+    {
+        [TestMethod()]
+        public void IdentifyFizzBuzzTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
