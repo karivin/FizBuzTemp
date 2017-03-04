fc988bd512292316f66b6fd9a665ec0b16340d71
Karishma <karivin>
2017-03-04 18:06:41 +0000

    IdentifyFizzBuzzTest_ValidNumberReturnBuzzNumberDivisibleBy5 pass - functionality implemented

--- FizBuzTemp/FizzBuzz.cs
+++ FizBuzTemp/FizzBuzz.cs
@@ -18,6 +18,8 @@ namespace FizBuzTemp
              return string.Empty;
             if (number%3 == 0)
                 return Fizz;
+            if (number%5 == 0)
+                return Buzz;
             return string.Empty;
         }
 
