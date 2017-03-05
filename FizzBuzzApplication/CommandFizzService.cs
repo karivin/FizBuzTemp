b3688a789e8a821acd0c7a3cdeb22d7ef4f66728
Karishma <karivin>
2017-03-05 12:58:25 +0000

    CommandFizzServiceTests.CanServiceFizzBuzzTest_NumberFizz_ReturnTrue failed as no implementation

---
+++ FizzBuzzApplication/CommandFizzService.cs
@@ -0,0 +1,15 @@
+﻿namespace FizzBuzzApplication
+{
+    public class CommandFizzService : CommandService, ICommandFizzBuzzService
+    {
+        public override bool CanServiceFizzBuzz(long number)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string ServiceFizzBuzz(long number)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ FizzBuzzApplicationTests/CommandFizzServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzApplication.Tests
+{
+    [TestClass()]
+    public class CommandFizzServiceTests
+    {
+        private ICommandService cmdService;
+        private long number = 3;
+        [TestMethod()]
+        public void CanServiceFizzBuzzTest_NumberFizz_ReturnTrue()
+        {
+            //Arrange
+            cmdService = new CommandFizzBuzzService();
+            bool result;
+
+            //Act
+            result = cmdService.CanServiceFizzBuzz(number);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+      }
+}
