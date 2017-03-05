b57fdfa7d01e470558474450c4faa6e9cf347bcc
Karishma <karivin>
2017-03-05 08:38:40 +0000

    create new project to implement commnand pattern n solid principles

---
+++ FizzBuzzApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a positive number(max 32 bit integer) : ");
+            long number = Convert.ToInt64(Console.ReadLine());
+            //implement fizzbuzz functionality
+            Console.ReadKey();
+        }
+    }
+}
