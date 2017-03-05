5024820b66d373a348af762917d111f346a98f35
Karishma <karivin>
2017-03-05 13:15:44 +0000

    CommandBuzzService.CanServiceFizzBuzz test fail as functionality not implemented

---
+++ FizzBuzzApplicationTests/CommandBuzzServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzApplication.Tests
+{
+    [TestClass()]
+    public class CommandBuzzServiceTests
+    {
+        private ICommandService cmdService;
+        private long number = 5;
+        [TestMethod()]
+        public void CanServiceFizzBuzzTest_NumberBuzz_ReturnTrue()
+        {
+            //Arrange
+            cmdService = new CommandBuzzService();
+            bool result;
+
+            //Act
+            result = cmdService.CanServiceFizzBuzz(number);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void ServiceFizzBuzzTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
